d101da93bfb7c9ad43de22a1f09384383faf9ff8
Todd Norton <toddmnorton@live.com>
2020-02-15 00:40:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each_with_index
+    deli.each_with_index(1) do |name, i|
   end
 end
 
