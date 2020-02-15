761fdef178f58c03a3291182a32876504eed3581
Todd Norton <toddmnorton@live.com>
2020-02-15 00:32:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.empty? 
+    "The line is currently empty."
+  else
+    "Hey look, customers!"
+  end
+end
+
+line(katz_deli)
